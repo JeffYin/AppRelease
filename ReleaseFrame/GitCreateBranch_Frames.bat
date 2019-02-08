317a0f1ce5d3cbcb09a36be04d3bffdfaf6848db
Jeff Yin <Jeff.Yin@powerschool.com>
2019-02-08 17:10:30 -0500

    Updated the branch number.

--- ReleaseFrame/GitCreateBranch_Frames.bat
+++ ReleaseFrame/GitCreateBranch_Frames.bat
@@ -1,5 +1,5 @@
-SET oldbranch=v.2018.11.00
-SET newBranch=v.2019.02.00
+SET oldbranch=v.2019.02.00
+SET newBranch=v.2019.04.00
 
 SET folders=GlobalConfig frame-common frame-dao frame-service frame-war frame-web
 FOR %%i IN (%folders%) DO (
@@ -8,10 +8,10 @@ FOR %%i IN (%folders%) DO (
     git checkout -b %newBranch% %oldbranch%
     git pull
     
-    ..\..\searchReplace.py pom.xml "<version>2018.11.00-SNAPSHOT</version>" "<version>2019.02.00-SNAPSHOT</version>"
+    ..\..\searchReplace.py pom.xml "<version>2019.02.00-SNAPSHOT</version>" "<version>2019.04.00-SNAPSHOT</version>"
 
     git add pom.xml
-    git commit -m "TRILL-6319 - Create the branches for 2019.02 release."
+    git commit -m "TRILL-7051 - Create the branches for 2019.04 release."
     git push origin %newBranch%
 
     cd ..
--- ReleaseFrame/GitSwitchBranch_Frames.bat
+++ ReleaseFrame/GitSwitchBranch_Frames.bat
@@ -1,9 +1,11 @@
-SET branchName=v.2018.07.00
+SET branchName=v.2019.02.00
 SET folders=GlobalConfig frame-common frame-dao frame-service frame-war frame-web
  
 FOR %%i IN (%folders%) DO (
     cd %%i
+    git pull
     git checkout %branchName%
+    git pull
     cd ..
 )
     
