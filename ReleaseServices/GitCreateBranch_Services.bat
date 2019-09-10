f5b604be47109b01a24792163e00e3f87cc9c84b
Jeff Yin <Jeff.Yin@powerschool.com>
2019-09-10 09:08:30 -0400

    Updated the GitCreateBranch_Services.bat

--- ReleaseServices/GitCreateBranch_Services.bat
+++ ReleaseServices/GitCreateBranch_Services.bat
@@ -1,32 +1,34 @@
 
-SET oldbranch=v.2019.06.00
-SET newBranch=v.2019.07.00
+SET oldbranch=tag.2019.04.00
+SET newBranch=v.2019.04.01
 
-SET folders=^
- service-achievement-elem ^
- service-achievement-sec ^
- service-attendance ^
- service-school ^
- service-staff ^
- service-student ^
- service-student-enrolment
+REM SET folders=^
+REM  service-achievement-elem ^
+REM  service-achievement-sec ^
+REM  service-attendance ^
+REM  service-school ^
+REM  service-staff ^
+REM  service-student ^
+REM  service-student-enrolment
+
+ SET folders=service-student
 
 FOR %%i IN (%folders%) DO (
     cd %%i
     git checkout %oldbranch%
     git pull
-    git checkout -b %newBranch% %oldbranch%
+    git checkout -b %newBranch%
     
     
     REM service version
-    python ..\..\searchReplace.py pom.xml "<service.version>2019.06.00</service.version>" "<service.version>2019.07.00</service.version>"
+    python ..\..\searchReplace.py pom.xml "<service.version>2019.04.00</service.version>" "<service.version>2019.04.01</service.version>"
 
     REM Frame Version
-    python ..\..\searchReplace.py pom.xml "<version>2019.06.00-SNAPSHOT</version>" "<version>2019.07.00-SNAPSHOT</version>"
+    python ..\..\searchReplace.py pom.xml "<version>2019.04.00-SNAPSHOT</version>" "<version>2019.04.01-SNAPSHOT</version>"
 
     git add pom.xml
-    git commit -m "TRILL-7647 Create the branch for 2019.07 release. "
-    git push origin %newBranch%
+    git commit -m "TRILL-8708 Create the branch for 2019.04.01 release. "
+    git push --set-upstream origin %newBranch%
 
     cd ..
 )
