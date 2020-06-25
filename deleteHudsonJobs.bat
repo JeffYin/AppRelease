f68341a716b910cd68d631d3c965fb68d063a7a6
Jeff Yin <Jeff.Yin@powerschool.com>
2020-06-25 13:25:27 -0400

    Build TwebSchool 5.43.20.

--- deleteHudsonJobs.bat
+++ deleteHudsonJobs.bat
@@ -1,12 +1,12 @@
-SET services=service-achievement-elem_v.2019.09.00 ^
-service-achievement-sec_v.2019.09.00 ^
-service-attendance_v.2019.09.00 ^
-service-school_v.2019.09.00 ^
-service-staff_v.2019.09.00 ^
-service-student-enrolment_v.2019.09.00 ^
-service-student_v.2019.09.00 
+SET services=service-achievement-elem_v.2020.01.00 ^
+service-achievement-sec_v.2020.01.00 ^
+service-attendance_v.2020.01.00 ^
+service-school_v.2020.01.00 ^
+service-staff_v.2020.01.00 ^
+service-student-enrolment_v.2020.01.00 ^
+service-student_v.2020.01.00 ^
+Frame_v.2020.01.00 
 
-REM Frame_v.2019.09.00 ^
 REM SchoolAdmin_5.42.00 
 
 
