a67e03c4578c292106d81c56f3ed92e7c7044187
Jeff Yin <Jeff.Yin@powerschool.com>
2020-10-19 15:36:07 -0400

    Frame was updated to 2020.11.01

--- ReleaseFrame/GitCreateTag_Frames.bat
+++ ReleaseFrame/GitCreateTag_Frames.bat
@@ -1,4 +1,4 @@
-SET versionToBeTagged=2020.08.00
+SET versionToBeTagged=2020.11.00
 
 SET branchName=v.%versionToBeTagged%
 SET tagName=tag.%versionToBeTagged%
--- ReleaseFrame/GitMerge_Frame.bat
+++ ReleaseFrame/GitMerge_Frame.bat
@@ -1,4 +1,4 @@
-SET branchSour=v.2020.08.00
+SET branchSour=v.2020.11.00
 SET branchDest=master
 
 SET folders=GlobalConfig frame-common frame-dao frame-service frame-war frame-web
