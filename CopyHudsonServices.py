972d62fe428bb1405da75f37ee039e1f1d2a29c5
Jeff Yin <Jeff.Yin@powerschool.com>
2021-05-11 10:36:22 -0400

    Updated to 2021.05.00

--- CopyHudsonServices.py
+++ CopyHudsonServices.py
@@ -4,9 +4,9 @@ import sys
 import os
 
 
-oldVer="v.2020.12.00"
-newVer="v.2021.03.00"
-newJobTab="2021.03"
+oldVer="v.2021.03.00"
+newVer="v.2021.05.00"
+newJobTab="2021.05"
 hudsonURL="http://trdev-001.srb-es.com:8080/hudson"
 # hudsonURL="http://localhost:8888/hudson"
 jobs=["service-achievement-elem", \
